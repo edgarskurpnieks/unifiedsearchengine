aa6944e21df2d5768538f4ad06823d27cb95eb44
Edgars Kurpnieks <edza1361@gmail.com>
2018-04-20 10:26:32 +0300

    db mock

---
+++ SearchLogic/ResultSetFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ultimate.Search.Processors;
+
+namespace Processors
+{
+    class ResultSetFilter
+    {
+        public static List<Document> FilterDocuments(List<Document> allDocs, string query)
+        {
+            return allDocs;
+        }
+    }
+}
--- SearchLogic/SearchLogic.cs
+++ SearchLogic/SearchLogic.cs
@@ -1,5 +1,6 @@
 ﻿using Elasticsearch.Net;
 using Nest;
+using Processors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,26 +25,11 @@ namespace Ultimate.Search.Processors
 
         public List<Document> DoSearch(string query)
         {
-            //var result = _client.Search<Document>(s => s
-            //    .Query(y => y
-            //        .MultiMatch(x => x
-            //            .Fields(p => p.Fields(l => l.Data, ls => ls.Test))
-            //            .Query(query)
-            //            )
-            //         )
-            //    .From(1)			
-            //    .Size(15));
-
-            var result = _client.Search<Document>(s => s
-               .Query(y => y
-                   .MatchAll(x => x
-                       .Name(query)
-                       )
-                    )
-               .From(1)
-               .Size(15));
-
-            return result.Documents.Select(x => new Document() { Data = x.Data, Test = x.Test }).ToList();
+            var allDocs = new List<Document>();
+            allDocs.AddRange(ResultSets.pdfDocuments);
+            allDocs.AddRange(ResultSets.caseDocuments);
+            var result = ResultSetFilter.FilterDocuments(allDocs, query);
+            return result.Select(x => new Document() { Data = x.Data, Test = x.Test }).ToList();
         }
     }
 }
