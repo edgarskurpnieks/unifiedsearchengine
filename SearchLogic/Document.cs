3d5d412dfbe4a7bafbe0ff4b7936525a93d3ef9c
Edgars Kurpnieks <edza1361@gmail.com>
2018-04-20 10:09:09 +0300

    fake document set

--- SearchLogic/Document.cs
+++ SearchLogic/Document.cs
@@ -6,6 +6,7 @@ namespace Ultimate.Search.Processors
 {
     public class Document
     {
+        public string objectType { get; set }
         public string Data { get; set; }
         public string Test { get; set; }
     }
---
+++ SearchLogic/ResultSets.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ultimate.Search.Processors;
+
+namespace Processors
+{
+    public static class ResultSets
+    {
+        public static List<Document> pdfDocuments = new List<Document>
+        {
+            new Document
+            {
+                objectType = "pdf",
+                Data = "Reginald Porter seeker gun-owner"
+            },
+            new Document
+            {
+                objectType = "pdf",
+                Data = "Abdullah Sayegh seeker military conflict"
+            },
+            new Document
+            {
+                objectType = "pdf",
+                Data = "Naif Asghar permit ex-military"
+            },
+            new Document
+            {
+                objectType = "pdf",
+                Data = "Amid Sarkis citizenship doctor"
+            },
+            new Document
+            {
+                objectType = "pdf",
+                Data = "Janis Jarans citizenship teacher"
+            }
+        };
+
+        public static List<Document> caseDocuments = new List<Document>
+        {
+            new Document
+            {
+                objectType = "case",
+                Data = "Reginald Porter "
+            },
+            new Document
+            {
+                objectType = "case",
+                Data = "Abdullah Sayegh"
+            },
+            new Document
+            {
+                objectType = "case",
+                Data = "Naif Asghar"
+            }
+        };
+    }
+}
